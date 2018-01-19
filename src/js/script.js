a581e7a6ee6a3cc6efc54f6baf61b4570c066404
samchery <chery.samantha@yahoo.fr>
2018-01-19 10:09:02 +0100

    animation JS

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,57 @@
-import { User } from './user';
+/* Counter hat left */
 
-console.log('Welcome !');
+var counter = document.querySelector('#count');
+setInterval(function(){
+    if(counter.innerHTML > 45765){
+        counter.innerHTML = counter.innerHTML - 2365;
+    } else{
+        return;
+    }
+  }, 0.001);
 
-let name = 'Bastien';
-const user = new User(name);
+
+/* ANiMATION YARN BALL */
+function getPosition(element) {
+    var xPosition = 0;
+    var yPosition = 0;
+
+    while(element) {
+        xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
+        yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
+        element = element.offsetParent;
+    }
+
+    return { x: xPosition, y: yPosition };
+}
+
+var yarnBall = document.querySelector('#yarn');
+var yarnBallPosition = getPosition(yarnBall);
+
+var fil = document.querySelector('#fil');
+
+var btnToGo = document.querySelector('.btn');
+var btnPosition = getPosition(btnToGo);
+
+
+setInterval(function(){
+    if(btnPosition.y > yarnBallPosition.y){
+        yarnBallPosition.y += 10;
+        yarnBall.style.top = yarnBallPosition.y + 'px';
+        fil.style.height = yarnBallPosition.y + 'px';
+        //yarnBall.className = "rotating";
+    } else{
+        btnToGo.className = "btn arrived";
+        //yarnBall.className = "";
+        return;
+    }
+}, 80);
+
+console.log(btnPosition);
+
+
+
+
+var t = 180;
+var n = document.getElementById("yarn");
+n.style.transform = "rotate(" + -t + "deg)";
+ 
